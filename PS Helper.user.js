289f4a4cd299214d3572e1124363d8740eddcd96
Ronny Jespersen <github@rjj-net.dk>
2020-07-12 22:10:15 +0700

    Initial commit

---
+++ PS Helper.user.js
@@ -0,0 +1,55 @@
+// ==UserScript==
+// @name         PS Helper
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Helper script for Profit Sniper
+// @author       TheNorthman
+// @match        https://www.tradingview.com/chart/*
+// @grant GM_setValue
+// @grant GM_getValue
+// @grant GM_setClipboard
+// @grant unsafeWindow
+// @grant window.close
+// @grant window.focus
+// ==/UserScript
+
+(function() {
+    'use strict';
+
+    // $(document).ready(function () {
+        console.log('start');
+    // });
+    // // var css = ".first-2n3flLMC { background: red !important; }"
+    // // var style = document.createElement("style");
+    // // style.type = "text/css";
+    // // style.appendChild(document.createTextNode(css));
+    // // document.head.appendChild(style);
+    
+    // $(document).ready(function () {
+    //     $('body').on('click', function () {
+    //         alert('hi');
+    //         setTimeout(() => {
+    //             // console.dir($("body").find('[data-dialog-name="BT Profit Sniper V3.0.22"]'));
+    //             var dialog = $("body").find('[data-dialog-name="BT Profit Sniper V3.0.22"]');
+    //             console.dir(dialog);
+    //             if (dialog.length) {
+
+    //             }
+    //             // $('.js-backtesting-open-format-dialog').css('style', 'background: red !important;');
+    //         }, 1000);
+    //         // console.log($('.wrapper-3ePvQMAQ').data('dialog-name')[0].substring(0, 16));
+    //     });
+    //                 pshelper.bt.init();
+    // });
+
+    // var pshelper = {
+    //     psEnable: false,
+    //     bt: {
+    //         init:function() {
+    //             pshelper.psEnable = true;
+    //             console.dir($('#overlap-manager-root .ellipsis-1PT8QOxq:contains("BT Profit Sniper V3.0.22")'));
+    //         }
+    //     }
+    // }
+    // alert('ok');
+})();
